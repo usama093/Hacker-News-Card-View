0feb47a83d5009c0df5449c45dc203bd77be0efb
Usama Afzal <usama.afzal.cg@gmail.com>
2019-12-01 23:30:27 +0100

    Add cutom hook

--- src/App.js
+++ src/App.js
@@ -1,44 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { CardView } from "./Components/cardView";
 import { NavBar } from "./Components/navBar";
 import { Spinner } from "./Components/spinner/spinner";
-import { getPostsIdUrl } from "./Utils/constants";
-import axios from "axios";
+import { usePosts } from "./Utils/getPosts";
 
 import "./App.css";
 
 function App() {
-  const [posts, setPosts] = useState();
-  const [isError, setIsError] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      let getPostsUrl = [];
-      let postPromises = [];
-      let postsId = await axios(getPostsIdUrl); //This API call will only return Id's of top stories
-      if (postsId.status >= 400) throw new Error("something went wrong");
-      postsId = postsId.data.slice(0, 24); // Only using partiall data for demo purpose otherwise Pagination should be used
-      getPostsUrl = postsId.map(postID => {
-        return `https://hacker-news.firebaseio.com/v0/item/${postID}.json`;
-      });
-      getPostsUrl.forEach(url => {
-        postPromises.push(axios.get(url));
-      });
-      // Using Axios concurrent requests to get posts
-      axios
-        .all(postPromises)
-        .then(
-          axios.spread((...posts) => {
-            setPosts(posts);
-          })
-        )
-        .catch(errors => {
-          setIsError(true);
-        });
-    };
-    fetchData();
-  }, []);
-
+  const [posts, isError] = usePosts();
   const postsList =
     posts &&
     posts.map(post => (
---
+++ src/Utils/getPosts.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import { getPostsIdUrl, baseUrl } from "./constants";
+import axios from "axios";
+
+export function usePosts() {
+  const [posts, setPosts] = useState();
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let getPostsUrl = [];
+      let postPromises = [];
+      let postsId = await axios(getPostsIdUrl); //This API call will only return Id's of top stories
+      if (postsId.status >= 400) throw new Error("something went wrong");
+      postsId = postsId.data.slice(0, 24); // Only using partiall data for demo purpose otherwise Pagination should be used
+      getPostsUrl = postsId.map(postID => {
+        return `${baseUrl}${postID}.json`;
+      });
+      getPostsUrl.forEach(url => {
+        postPromises.push(axios.get(url));
+      });
+      // Using Axios concurrent requests to get posts
+      axios
+        .all(postPromises)
+        .then(
+          axios.spread((...posts) => {
+            setPosts(posts);
+          })
+        )
+        .catch(errors => {
+          console.log(errors);
+          setIsError(true);
+        });
+    };
+    fetchData();
+  }, []);
+
+  return [posts, isError];
+}
